57d1f2f719ba6668cd33e6304b9b52a6cba9e382
haidi20 <nhaidii@yahoo.com>
2019-09-02 20:57:00 +0800

    menambahkan-file-json-dummy

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,17 +1,26 @@
 import React, {Component, Fragment} from 'react';
 import './BlogPost.css';
 import Post from '../../component/Post/Post';
+import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
         post: []
     }
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => {
-            this.setState ({
-                post: json
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => response.json())
+        // .then(json => {
+        //     this.setState ({
+        //         post: json
+        //     })
+        // })
+
+        axios.get('http://localhost:3004/posts')
+        .then((res) => {
+            console.log(res.data);
+            this.setState({
+                post: res.data
             })
         })
     }
