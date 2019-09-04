26bde59d6186a4c51e96cbdb0e6a7c15026552d6
haidi20 <nhaidii@yahoo.com>
2019-09-04 20:20:08 +0800

    perbaikan-struktur-folder

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,65 +1,20 @@
 import React, {Component, Fragment} from 'react';
 
 // import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-import Product from '../Product/Product';
-import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
-import BlogPost from '../BlogPost/BlogPost';
+import Product from '../Pages/Product/Product';
+import LifeCycleComp from '../Pages/LifeCycleComp/LifeCycleComp';
+import BlogPost from '../Pages/BlogPost/BlogPost';
+
+import './Home.css';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Home extends Component {
-    state = {
-        showComponent: true
-    }
-    componentDidMount(){
-        // setTimeout(() => {
-        //     this.setState({
-        //         showComponent: false
-        //     })
-        // }, 5000)
-       
-    }
     render(){
         return (
             <Router>
-                <div>
-                    {/* <p>YouTube Component</p>
-                    <hr/>
-                    <YouTubeComp 
-                        time="7.12" 
-                        title="react pertama"
-                        desc="masih mudah"/>
-                    <YouTubeComp 
-                        time="8.02" 
-                        title="react tengah"
-                        desc="lumayan susah"/>
-                    <YouTubeComp 
-                        time="5.04" 
-                        title="react hampir selesai"
-                        desc="susah sekali"/>
-                    <YouTubeComp 
-                        time="4.12" 
-                        title="react selesai"
-                        desc="akhirnya bisa"/>
-                    <YouTubeComp /> */}
-
-                    {/* <p>Counter</p>
-                    <hr/>
-                    <Product/> */}
-
-                    {/* <p>LifeCycle Component</p>
-                    <hr/>
-                    {
-                        this.state.showComponent ? <LifeCycleComp /> : null
-                    } */}
-
-                    {/* <p>Blog Post</p>
-                    <hr/>
-
-                    <BlogPost /> */}
-                </div>
                 <Fragment>
-                    <div>
+                    <div className="navigation">
                         <Link to="/">Blog Post</Link>
                         <Link to="/product">Product</Link>
                         <Link to="/lifecycle">LifeCycle</Link>
