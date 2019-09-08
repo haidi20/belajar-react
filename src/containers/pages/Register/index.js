c5fb570e0cefc3cf491eb8cb4a904e9ac6f06f81
haidi20 <nhaidii@yahoo.com>
2019-09-08 16:23:36 +0800

    bisa-melakukan-registrasi

--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,15 +1,49 @@
 import React, {Component} from 'react';
 import './Register.scss';
+import firebase from '../../../config/firebase';
 
 class Register extends Component{
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleChangeText = (e) => {
+        // console.log(e.target.name)
+        this.setState({
+            [e.target.id]: e.target.value,
+        })
+    }
+
+    handleRegisterSubmit = () => {
+        console.log(this.state.email);
+        console.log(this.state.password);
+
+        const {email, password} = this.state;
+        console.log('data sebelum di kirim', email, password);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            
+            console.log(errorCode, errorMessage);
+
+        });          
+    }
+
     render(){
         return(
             <div className="auth-container">
                 <div className="auth-card">
                     <p className="auth-title">Register Page</p>
-                    <input className="input" placeholder="email" type="text" />
-                    <input className="input" placeholder="password" type="password" />
-                    <button className="btn">register</button>
+                    <input className="input" name="email" id="email" placeholder="email" type="text" onChange={this.handleChangeText}/>
+                    <input className="input" name="password" id="password" placeholder="password" type="password" onChange={this.handleChangeText}/>
+                    <button className="btn" onClick={this.handleRegisterSubmit} >register</button>
                     {/* <button>Go to dashboard</button> */}
                 </div>
             </div>
