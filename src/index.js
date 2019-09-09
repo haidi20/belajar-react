c4291b0e4ad6d862e6affdf46d38b3186623ea0c
haidi20 <nhaidii@yahoo.com>
2019-09-09 20:16:43 +0800

    first-learn-course-react-udemy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/pages/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
