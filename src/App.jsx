c86bce012dfb2d3cd6dce126e6ce709a61c4e5a5
haidi20 <nhaidii@yahoo.com>
2019-09-10 09:17:42 +0800

    make-button-calculator

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Button from './components/Buttons';
+// import logo from '../../../assets/img/logo/logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      current: '0',
+      previous: []
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      result: '0'
+    })
+  }
+
+  addToCurrent = (symbol) => {
+    this.setState({
+      current: this.state.current + symbol
+    })
+  }
+
+  render(){
+    const buttons = [
+      {symbol: 'C', cols: 3, action: this.reset},
+      {symbol: '/', cols: 1, action: this.addToCurrent},
+      {symbol: '7', cols: 1, action: this.addToCurrent},
+      {symbol: '8', cols: 1, action: this.addToCurrent},
+      {symbol: '9', cols: 1, action: this.addToCurrent},
+      {symbol: 'x', cols: 1, action: this.addToCurrent},
+      {symbol: '4', cols: 1, action: this.addToCurrent},
+      {symbol: '5', cols: 1, action: this.addToCurrent},
+      {symbol: '6', cols: 1, action: this.addToCurrent},
+      {symbol: '-', cols: 1, action: this.addToCurrent},
+      {symbol: '1', cols: 1, action: this.addToCurrent},
+      {symbol: '2', cols: 1, action: this.addToCurrent},
+      {symbol: '3', cols: 1, action: this.addToCurrent},
+      {symbol: '+', cols: 1, action: this.addToCurrent},
+      {symbol: '0', cols: 2, action: this.addToCurrent},
+      {symbol: '.', cols: 1, action: this.addToCurrent},
+      {symbol: '=', cols: 1, action: this.addToCurrent},
+    ]
+    return (
+      <div className="App">
+        <input className="result" type="text" value={this.state.current}></input>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
