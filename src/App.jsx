0adaddb952f440b0a536b86428c71d1e27709e73
haidi20 <nhaidii@yahoo.com>
2019-09-10 14:36:39 +0800

    learn-routing-website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
-// import logo from '../../../assets/img/logo/logo.svg';
+import PageWrapper from './components/PageWrapper';
+import Home from './components/pages/Home';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
 
   render(){
     return (
-      <h2>Hello World</h2>
+      <Router>
+        <PageWrapper>
+          <Route 
+              path="/home" 
+              component={Home}
+            />
+        </PageWrapper>
+      </Router>
+      
     );
   }
 }
