cc5bc8758c0c4fe827aa048dedd5f65674f16759
haidi20 <nhaidii@yahoo.com>
2019-09-13 11:48:41 +0800

    tutorial-1

---
+++ src/components/TrelloCard.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// ui
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
+const TrelloCard = () => {
+    return(
+        <Card >
+            <Typography >
+                Word Of Day    
+            </Typography>
+        </Card>
+    )
+}
+
+export default TrelloCard;
