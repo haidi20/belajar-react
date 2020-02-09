9900fb3f4e84bf90d3d33bead58b9abfbb2e998a
haidi <haidinurhadinata@gmail.com>
2020-02-09 18:01:23 +0800

    berhasil memunculkan tampilan burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Auxt from '../../hoc/Auxt';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component{
     render(){
         return(
             <div>
                 <Auxt>
-                    <div>Burger</div>
+                    <Burger />
                     <div>Build Controls</div>
                 </Auxt>
             </div>
